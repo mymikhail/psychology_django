9b0984ec26bb9bda54ed70abcfeaf7d1ef1150ca
mymikhail <taskmath80@gmail.com>
2014-08-10 00:52:03 +0400

    ee

--- psychology/models.py
+++ psychology/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Test(models.Model):
-    test_id = models.AutoField(max_length=10)
+    test_id = models.AutoField(max_length=10, primary_key=True)
     title = models.CharField(max_length=250)
     description = models.CharField()
 
@@ -12,19 +12,19 @@ class AnalyzingTest(models.Model):
     description = models.CharField()
 
 class Question(models.Model):
-    question_id = models.AutoField(max_length=10)
+    question_id = models.AutoField(max_length=10, primary_key=True)
     description = models.CharField(max_length)
     order = models.IntegerField(max_length=3)
     test_id = models.ForeignKey(Test)
 
 class Variation(models.Model):
-    variation_id = models.AutoField(max_length=10)
+    variation_id = models.AutoField(max_length=10, primary_key=True)
     question_id = models.ForeignKey(Question)
     variation = models.CharField(max_length=255)
     value = models.IntegerField(max_length=4)
 
 class User(models.Model):
-    user_id = models.AutoField(max_length=10)
+    user_id = models.AutoField(max_length=10, primary_key=True)
     login = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     name = models.CharField(max_length=200)
@@ -32,7 +32,7 @@ class User(models.Model):
     email = models.CharField(max_length=100)
 
 class Testing(models.Model):
-    testing_id = models.AutoField(max_length=10)
+    testing_id = models.AutoField(max_length=10, primary_key=True)
     test_id = models.ForeignKey(Test)
     user_id = models.ForeignKey(User)
     date_created = models.DateTimeField()
