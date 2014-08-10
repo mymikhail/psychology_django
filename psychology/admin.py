2d0ea53dbe990f5ae1ddb567d8e80929c9928713
mymikhail <taskmath80@gmail.com>
2014-08-10 21:31:13 +0400

    new admin

--- psychology/admin.py
+++ psychology/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from psychology.models import Test
+from psychology.models import Test, AnalyzingTest
 
-admin.site.register(Test)
+
+class AnalyzingTestInline(admin.StackedInline):
+    model = AnalyzingTest
+    extra = 3
+
+class TestAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Название теста',	{'fields': ['title']}),
+        ('Описание', 		{'fields': ['description'], 'classes': ['collapse']}),
+    ]
+    inlines = [AnalyzingTestInline]
+
+admin.site.register(Test, TestAdmin)
 # Register your models here.
--- psychology/models.py
+++ psychology/models.py
@@ -5,11 +5,17 @@ class Test(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
 
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.title    
+
 class AnalyzingTest(models.Model):
     test_id = models.ForeignKey(Test)
     num1 = models.IntegerField(max_length=5)
     num2 = models.IntegerField(max_length=5)
     description = models.TextField()
+
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.description
 
 class Question(models.Model):
     question_id = models.AutoField(max_length=10, primary_key=True)
@@ -17,11 +23,17 @@ class Question(models.Model):
     order = models.IntegerField(max_length=3)
     test_id = models.ForeignKey(Test)
 
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.description    
+
 class Variation(models.Model):
     variation_id = models.AutoField(max_length=10, primary_key=True)
     question_id = models.ForeignKey(Question)
     variation = models.CharField(max_length=255)
     value = models.IntegerField(max_length=4)
+
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.variation    
 
 class User(models.Model):
     user_id = models.AutoField(max_length=10, primary_key=True)
@@ -30,6 +42,9 @@ class User(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     email = models.CharField(max_length=100)
+
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.login    
 
 class Testing(models.Model):
     testing_id = models.AutoField(max_length=10, primary_key=True)
@@ -40,4 +55,3 @@ class Testing(models.Model):
 class UserTesting(models.Model):
     testing_id = models.ForeignKey(Testing)
     variation_id = models.ForeignKey(Variation)
-
