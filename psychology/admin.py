14a8d13678f2233772adefa95527c4f70b0c2e1d
mikhail <mikhail@mybox>
2014-08-10 19:29:10 +0200

    ignore

--- psychology/admin.py
+++ psychology/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from psychology.models import Test
+from psychology.models import AnalyzingTest
+
 
 admin.site.register(Test)
+admin.site.register(AnalyzingTest)
 # Register your models here.
