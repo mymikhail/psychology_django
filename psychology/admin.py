b05da7483c69435bb88364cb379eceecdf47a8a6
mymikhail <taskmath80@gmail.com>
2014-08-10 22:30:10 +0400

    other models

--- psychology/admin.py
+++ psychology/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from psychology.models import Test, AnalyzingTest
+from psychology.models import Test, AnalyzingTest, Question, Variation, User
 
 
 class AnalyzingTestInline(admin.TabularInline):
@@ -9,9 +9,23 @@ class AnalyzingTestInline(admin.TabularInline):
 class TestAdmin(admin.ModelAdmin):
     fieldsets = [
         ('title',	{'fields': ['title']}),
-        ('description', 		{'fields': ['description'], 'classes': ['collapse']}),
+        ('description', {'fields': ['description'], 'classes': ['collapse']}),
     ]
     inlines = [AnalyzingTestInline]
 
+class VariationInline(admin.TabularInline):
+    model = Variation
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('order',	{'fields': ['order']}),
+        ('description', {'fields': ['description'], 'classes': ['collapse']}),
+    ]
+    inlines = [VariationInline]
+
+
 admin.site.register(Test, TestAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(User)
 # Register your models here.
